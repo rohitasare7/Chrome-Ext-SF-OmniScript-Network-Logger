389f288cad973e2801892a4be84f345164c98cf6
RohitkumarA <rohitasare7@gmail.com>
2024-12-14 16:25:17 +0530

    Added how to image in popup

--- src/popup.vue
+++ src/popup.vue
@@ -20,6 +20,12 @@ body {
   <div class="p-5 bg-gray-100 antialiased" style="max-width: 700px; min-width: 600px;">
     <div class="bg-white rounded-2xl p-6">
       <h2 class="text-xl font-bold mb-4 text-gray-800">Salesforce OmniStudio Network Logger</h2>
+      <div class="py-2 mt-2 border-t">
+        <TextDesc class="my-2 text-gray-700">To use this extension, before your network requests are initialized, please open the network logger by "Right Click" then "Inspect" and then "OS
+          Network Logger" tab (Check below screenshot)
+        </TextDesc>
+        <img src="./assets/img/how_to_network_ss.png" class="rounded-lg border border-gray-300 p-4" />
+      </div>
       <!-- <div class="flex items-center justify-start py-2 mt-2 border-t dark:border-gray-500">
       <a class="text-blue-700 font-semibold mr-1" :href="webStoreURL" target="_blank">
         <button class="bg-blue-700 hover:bg-blue-800 text-white py-2 px-4 rounded-3xl mr-2">Web Store
@@ -31,9 +37,8 @@ body {
     </div> -->
       <TextDesc class="my-2 text-gray-700">If you'd like to show your support, please consider
         checking out to Author's YouTube Channel :
-        <a class="text-blue-600 dark:text-blue-300 font-semibold" href="https://www.youtube.com/@ThatSalesforceGuy"
-          target="_blank">That Salesforce Guy</a> or you can also support via <a
-          class="text-blue-600 dark:text-blue-300 font-semibold"
+        <a class="text-blue-600 font-semibold" href="https://www.youtube.com/@ThatSalesforceGuy" target="_blank">That
+          Salesforce Guy</a> or you can also support via <a class="text-blue-600 font-semibold"
           href="https://patreon.com/thatsalesforceguy?utm_medium=unknown&utm_source=join_link&utm_campaign=creatorshare_creator&utm_content=copyLink"
           target="_blank">Patreon</a>
       </TextDesc>
